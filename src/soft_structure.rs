07582158bd358868c504916b8a739e5552bc662a
berkealgul <berkealgul@gmail.com>
2023-09-25 20:48:48 +0300

    progress on initialization

--- src/soft_structure.rs
+++ src/soft_structure.rs
@@ -13,11 +13,14 @@ impl SoftMesh  {
         for vertex in self.vertex_vec.iter() {
             vertex.draw()
         }
+        for edge in self.edge_vec.iter() {
+            edge.draw()
+        }
     }    
 
     pub fn init(&mut self, w:usize, h: usize)
     {
-        let mut vertex_grid: Vec<Vec<Option<*mut Vertex>>> = vec![vec![None; w]; h];
+        //let mut vertex_grid: Vec<Vec<Option<*mut Vertex>>> = vec![vec![None; w]; h];
 
         for i in 1..w{
             for j in 1..h{
@@ -25,7 +28,23 @@ impl SoftMesh  {
                 let j_ = f32::from(j as i16 * 10);
                 let mut v: Vertex = Vertex { pos: Vec3 { x: i_, y: j_, z: 0.0 } };
                 self.vertex_vec.push(v);
-                vertex_grid[i][j] = Some(&mut v);
+                
+                let mut prev_vec: Vertex;
+
+                if i > 1 {
+                    prev_vec = self.vertex_vec[(j * w) + i - 1];  
+                    self.edge_vec.push(Edge{v1:v, v2: prev_vec})
+                }
+                if j > 1 {
+                    prev_vec = self.vertex_vec[((j-1) * w) + i]; 
+                    self.edge_vec.push(Edge{v1:v, v2: prev_vec})  
+                }
+                if i > 1 && j > 1 {
+                    prev_vec = self.vertex_vec[((j-1) * w) + i - 1];    
+                    self.edge_vec.push(Edge{v1:v, v2: prev_vec})
+                }
+                
+                //vertex_grid[i][j] = Some(&mut v);
             }
         }
     }
@@ -34,9 +53,15 @@ impl SoftMesh  {
 
 #[derive(Debug, Clone, Copy)]
 pub struct Edge {
-
+    pub v1: Vertex,
+    pub v2: Vertex
 }
 
+impl Edge {
+    pub fn draw(&self) {
+        draw_line(self.v1.pos.x, self.v1.pos.y, self.v2.pos.x, self.v2.pos.y, 2.0, BLACK);
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 pub struct Vertex {
